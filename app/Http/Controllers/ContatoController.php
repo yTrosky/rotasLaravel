bec1f8ea31cb357cfaff2af7b2010fe00d832d08
yTrosky <thiago.machado2498@gmail.com>
2023-09-25 08:03:34 -0300

    atualização

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato() {
-        var_dump($_GET);
+        var_dump($_POST);
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
